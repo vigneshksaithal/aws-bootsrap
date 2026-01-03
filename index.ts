64088c5d8d94e3bc5930200e52f3575f170fd6a8
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2026-01-03 10:53:16 +0530

    Create basic hello world application

---
+++ index.ts
@@ -0,0 +1,13 @@
+import {} from "bun";
+
+const message = "Hello World\n";
+const port = 8080;
+
+const server = Bun.serve({
+    port: 8080,
+    routes: {
+        "/": () => new Response(message),
+    },
+});
+
+console.log(`Listening on ${server.url}:${server.port}`);
